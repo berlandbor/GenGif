7c042cedf3be9d5cf4ea549eb9cb7fc66da7d03c
Andrew Berlandbor <141494909+berlandbor@users.noreply.github.com>
2025-01-30 20:32:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function addFrame() {
     tempCtx.drawImage(canvas, 0, 0);
 
     if (text) {
-        tempCtx.font = "5px Georgia";
+        tempCtx.font = "10px Georgia";
         tempCtx.fillStyle = "white";
         tempCtx.strokeStyle = "black";
         tempCtx.lineWidth = 3;
